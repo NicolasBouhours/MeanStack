bd1b657012132706c3e3b0a3624a75dfdf297de9
NicolasBouhours <nico53960@hotmail.fr>
2016-11-29 21:55:14 +0100

    STEP 27 - Rendering an Angular 2 App with NodeJS/Express

--- routes/app.js
+++ routes/app.js
@@ -1,27 +1,8 @@
 let express = require('express');
 let router = express.Router();
-let User = require('../models/user');
 
 router.get('/', (req, res, next) => {
-    User.findOne({}, (err, user) => {
-        if (err) {
-            return res.send('Error');
-        }
-        res.render('node', {email: user.email});
-    });
-});
-
-router.post('/', (req, res, next) => {
-    let email = req.body.email;
-    let user = new User({
-        firstName: 'Max',
-        lastName: 'Schwarz',
-        password: 'super-secret',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
-
+    res.render('index');
 });
 
 module.exports = router;
