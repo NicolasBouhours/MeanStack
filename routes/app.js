fef6f47761ac2d7279b5948ced06d7f1619a8cb9
NicolasBouhours <nico53960@hotmail.fr>
2016-11-29 21:14:38 +0100

    STEP 11 - Creating Routes and Passing Data

--- routes/app.js
+++ routes/app.js
@@ -5,8 +5,14 @@ router.get('/', (req, res, next) => {
     res.render('index');
 });
 
-router.get('/message', (req, res, next) => {
-    res.render('node', {message: 'Hello!'});
+router.get('/message/:msg', (req, res, next) => {
+    res.render('node', {message: req.params.msg});
+});
+
+router.post('/message', (req, res, next) => {
+    let message = req.body.message;
+    res.redirect('/message/' + message);
+
 });
 
 module.exports = router;
