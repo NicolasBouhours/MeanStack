7ee79814db93be3a64b43b797c3e11b933d5fba8
= <=>
2016-11-30 13:47:43 +0100

    STEP 55 - Adding Child Routes

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,11 +1,13 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { AUTH_ROUTES } from './auth/auth.routes';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { MessagesComponent } from './messages/messages.component';
-import { RouterModule, Routes } from '@angular/router';
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/messages', pathMatch: 'full' },
     { path: 'messages', component: MessagesComponent },
-    { path: 'auth', component: AuthenticationComponent }
+    { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-authentication',
     template: `
-        <h1>Authentication</h1>
+        <header class="row spacing">
+            <ul class="col-md-8 col-md-offset-2">
+                <ul class="nav nav-tabs">
+                    <li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>
+                    <li><a [routerLink]="['signin']">Signin</a></li>
+                    <li><a [routerLink]="['logout']">Logout</a></li>
+                </ul>
+            </ul>    
+        </header>
+        <div class="row spacing">
+            <router-outlet></router-outlet>
+        </div>
     `
 })
 export class AuthenticationComponent {
