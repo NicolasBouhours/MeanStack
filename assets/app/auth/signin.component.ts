6040285653d7a5bc058e78128732e3894eb36c0e
NicolasBouhours <nico53960@hotmail.fr>
2016-11-30 21:46:23 +0100

    STEP 57 - Finishing the Frontend

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
 export class SigninComponent {
-    
+    myForm: FormGroup;
+
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern('^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$')
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
+
+    onSubmit() {
+        console.log(this.myForm);
+        this.myForm.reset();
+    } 
 }
--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,18 +1,32 @@
+import { MessageService } from './message.service';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Message } from './message.models';
 
 @Component({
     selector: 'app-message',
-    templateUrl: './message.component.html'
+    templateUrl: './message.component.html',
+    styles: [`
+        .author, .config {
+            display: inline-block;
+        }
+
+        .config {
+            float: right;
+        }
+    `]
 })
 export class MessageComponent {
     @Input() message: Message;
     @Output() editClicked = new EventEmitter<string>();
 
-    color: 'red';
+    constructor(private messageService: MessageService) {}
 
-    onClick() {
+    onEdit() {
         alert('it worked');
         this.editClicked.emit('A new value')
+    }
+
+    onDelete() {
+        this.messageService.deleteMessage(this.message);
     }
 }
