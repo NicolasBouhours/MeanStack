f0952f4da992e5a30a7fa2e3a1e2cf0b971c2649
= <=>
2016-11-30 13:20:40 +0100

    STEP 50 - Creating a Header & Restructuring the Project

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
+import { MessagesComponent } from './messages/messages.component';
 import { MessageComponent } from './messages/message.component';
 import { MessageListComponent } from './messages/message-list.component';
 import { MessageInputComponent } from './messages/message-input.component';
+import { HeaderComponent } from './header.component';
+import { AuthenticationComponent } from './auth/authentication.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        MessagesComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     providers: [MessageService],
---
+++ assets/app/auth/authentication.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-authentication',
+    template: `
+        <div class="container">
+            authentication
+        </div>   
+    `
+})
+export class AuthenticationComponent {
+
+}
