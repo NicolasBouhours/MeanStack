5f73b46b17b56e79449b77c292c6681ca657d185
NicolasBouhours <nico53960@hotmail.fr>
2016-11-30 22:01:18 +0100

    STEP 59 - Backend : Saving Messages

---
+++ routes/message.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let router = express.Router();
+
+let Message = require('../models/message');
+
+router.get('/', (req, res, next) => {
+    let message = new Message({
+        content: req.body.content,
+    });
+
+    message.save((err, result) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+        res.status(201).json({
+            message: 'Saved message',
+            obj: result
+        });
+    });
+});
+
+module.exports = router;
