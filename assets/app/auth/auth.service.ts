5451121fc9bed5901ea0e091b7167c83ba48a0fc
NicolasBouhours <nico53960@hotmail.fr>
2016-12-01 22:53:21 +0100

    STEP 73 - User Sign In : Wiring up Backend and Frontend

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -16,4 +16,12 @@ export class AuthService {
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
     }
+
+    signin(user: User) {
+        const body = JSON.stringify(user);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/user/signin', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
 }
--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
+import { User } from './user.models';
 
 @Component({
     selector: 'app-signin',
@@ -7,6 +11,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SigninComponent {
     myForm: FormGroup;
+
+    constructor(private authService: AuthService, private router : Router) { }
 
     ngOnInit() {
         this.myForm = new FormGroup({
@@ -19,7 +25,17 @@ export class SigninComponent {
     }
 
     onSubmit() {
-        console.log(this.myForm);
+        const user = new User(this.myForm.value.email, this.myForm.value.password);
+        this.authService.signin(user)
+            .subscribe(
+                data => {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('userId', data.userId);
+                    this.router.navigateByUrl('/');
+                },
+                error => console.log(error)
+        );
+
         this.myForm.reset();
     } 
 }
