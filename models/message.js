7dc81d1a81d33f4f0ba8302ba69b2c778a01eee9
NicolasBouhours <nico53960@hotmail.fr>
2016-12-01 23:18:29 +0100

    STEP 78 - Connecting Users with Messages

--- models/message.js
+++ models/message.js
@@ -1,9 +1,18 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+let User = require('./user');
+
 let schema = new Schema({
     content: {type: String, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+schema.post('remove', (message) => {
+    User.findById(message.user, (err, user) => {
+        user.messags.pull(message);
+        user.save();
+    });
 });
 
 module.exports = mongoose.model('Message', schema);
--- routes/message.js
+++ routes/message.js
@@ -3,6 +3,7 @@ let jwt = require('jsonwebtoken');
 let router = express.Router();
 
 let Message = require('../models/message');
+let User = require('../models/user');
 
 router.get('/', (req, res, next) => {
     Message.find().exec((err, messages) => {
@@ -32,20 +33,31 @@ router.use('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    let message = new Message({
-        content: req.body.content,
-    });
-
-    message.save((err, result) => {
+    let decoded = jwt.decode(req.query.token);
+    User.findById(decoded.user._id, (err, user) {
         if (err) {
             return res.status(500).json({
                 title: 'An error occured',
                 error: err
             });
         }
-        res.status(201).json({
-            message: 'Saved message',
-            obj: result
+        let message = new Message({
+            content: req.body.content,
+            user: user
+        });
+        message.save((err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occured',
+                    error: err
+                });
+            }
+            user.messages.push(result);
+            user.save();
+            res.status(201).json({
+                message: 'Saved message',
+                obj: result
+            });
         });
     });
 });
