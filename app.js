3582f1331e1bec18160ba0c97dea6a67a9254949
NicolasBouhours <nico53960@hotmail.fr>
2016-12-03 09:13:06 +0100

    STEP 96 - Setting up MongoLab to host the MongoDB

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var messageRoutes = require('./routes/message');
 var userRoutes = require('./routes/user');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+//mongoose.connect('localhost:27017/node-angular');
+mongoose.connect('mongodb://test-user:test_pw@ds119568.mlab.com:19568/angular2-deployment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
